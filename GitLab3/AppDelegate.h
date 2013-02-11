49f0d4dbfe20c0be672503bc8d1483a065c3d3a3
mark <mark@patristar.com>
2013-02-11 23:28:35 +0800

    Initial Commit

---
+++ GitLab3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitLab3
+//
+//  Created by Ho Mark on 13/2/11.
+//  Copyright (c) 2013年 Ho Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitLab3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitLab3
+//
+//  Created by Ho Mark on 13/2/11.
+//  Copyright (c) 2013年 Ho Mark. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
